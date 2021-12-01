41fe46d5b074e62929e7da3ce0d35081cf80caca
Shri Genesis <rsmicky9@gmail.com>
2021-12-01 12:09:25 +0530

    add log print funcation

--- mylibrary/src/main/java/com/example/mylibrary/AlarmActivity.java
+++ mylibrary/src/main/java/com/example/mylibrary/AlarmActivity.java
@@ -1,11 +1,13 @@
 package com.example.mylibrary;
 
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,7 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button start= findViewById(R.id.button);
       /*  start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,21 +29,24 @@ public class AlarmActivity extends AppCompatActivity {
                 startAlert();
             }
         });*/
+    //    startAlert("",getApplicationContext());
 
     }
-    public void startAlert(){
-        EditText text = findViewById(R.id.time);
-        int i = Integer.parseInt(text.getText().toString());
-        Intent intent = new Intent(this, MyBroadcastReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                this.getApplicationContext(), 234324243, intent, 0);
+    public void startAlert(Activity context, String value){
+
+        int i = Integer.parseInt(value);
+        Intent intent = new Intent(context, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 234324243, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
-                + (i * 1000), pendingIntent);
-        Toast.makeText(this, "Alarm set in " + i + " seconds",Toast.LENGTH_LONG).show();
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (i * 1000), pendingIntent);
+        Toast.makeText(context, "Alarm set in " + i + " seconds",Toast.LENGTH_LONG).show();
     }
 
     public void setMsgToast(Context context, String msg){
         Toast.makeText(context, "Welcome: )"+msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void printLog(String tag,String value){
+        Log.d(tag,value);
     }
 }
